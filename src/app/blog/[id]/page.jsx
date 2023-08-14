17d00f455a0bbcd6bf42b0f87331686875d8e0d5
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2023-08-14 15:25:59 +0200

    Metatags

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -34,6 +34,61 @@ function extractTextNodes(content) {
   return texts;
 }
 
+export async function generateMetadata({ params }) {
+  await store.dispatch(fetchSinglePost(params.id));
+  const article = store.getState().posts.currentPost;
+  const parsedBody = JSON.parse(article.body);
+  const articleBody = extractTextNodes(parsedBody.root);
+  const truncatedArticleDescription = _.truncate(articleBody, {
+    length: 150,
+    omission: "...",
+  });
+  let modifiedArticle = { ...article };
+
+  modifiedArticle.date = new Date(
+    article.date.seconds * 1000 + article.date.nanoseconds / 1000000
+  ).toLocaleDateString();
+  modifiedArticle.body = JSON.parse(article.body);
+
+  return {
+    title: article.title,
+    description: truncatedArticleDescription,
+    openGraph: {
+      title: article.title,
+      description: truncatedArticleDescription,
+      type: "article",
+      publishedTime: modifiedArticle.date,
+      authors: modifiedArticle.author,
+      url: `https:/matteo-dirollo.com/blog/${params.id}`,
+      siteName: "Matteo's Portfolio & Blog",
+      images: [
+        {
+          url: article.imageUrl,
+          width: 800,
+          height: 600,
+        },
+        {
+          url: article.imageUrl,
+          width: 1800,
+          height: 1600,
+          alt: "3D rendering of abstract shapes created with Blender Cycles, featuring a machine resembling a pendulum.",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: 'Next.js',
+      description: 'The React Framework for the Web',
+      siteId: '1467726470533754880',
+      creator: '@nextjs',
+      creatorId: '1467726470533754880',
+      images: ['https://nextjs.org/og.png'],
+    },
+  };
+}
+
 export default async function Article({ params }) {
   await store.dispatch(fetchSinglePost(params.id));
   const article = store.getState().posts.currentPost;
@@ -45,44 +100,26 @@ export default async function Article({ params }) {
   });
 
   let modifiedArticle = { ...article };
-  
-  modifiedArticle.date = new Date(article.date.seconds * 1000 + article.date.nanoseconds / 1000000
+
+  modifiedArticle.date = new Date(
+    article.date.seconds * 1000 + article.date.nanoseconds / 1000000
   ).toLocaleDateString();
   modifiedArticle.body = JSON.parse(article.body);
 
   return (
     <div>
-      <Head>
-        {/* <!-- Open Graph Tags --> */}
-        <meta property="og:title" content={modifiedArticle.id} />
-        <meta property="og:description" content={truncatedArticleDescription} />
-        <meta property="og:image" content={modifiedArticle.imageUrl} />
-        <meta property="og:type" content="article" />
-        <meta
-          property="og:url"
-          content={`https:matteo-dirollo/blog/${modifiedArticle.id}`}
-        />
-        <meta property="og:site_name" content="mdr" />
-
-        {/* <!-- Twitter Card Tags --> */}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@matteodirollo" />
-        <meta name="twitter:title" content={modifiedArticle.title} />
-        <meta
-          name="twitter:description"
-          content={truncatedArticleDescription}
-        />
-        <meta name="twitter:image" content={modifiedArticle.imageUrl} />
-
-        <title>{modifiedArticle.title}</title>
-      </Head>
       <Container
         my={10}
         align="stretch"
         maxW={["fit-content", "80%"]}
         style={{ overflowX: "hidden" }}
       >
-        <Box as="article" key={modifiedArticle.id} maxW={"1000px"} margin={"auto"}>
+        <Box
+          as="article"
+          key={modifiedArticle.id}
+          maxW={"1000px"}
+          margin={"auto"}
+        >
           <ArticleHeading title={modifiedArticle.title} />
           <Subtitle article={modifiedArticle} />
           <Center>
