bd49d32a33e66c0c5628273b7af29d5cf31b5a19
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2023-08-21 22:07:57 +0200

    Icons for the lexical editor are visible again

--- src/app/(public)/layout.jsx
+++ src/app/(public)/layout.jsx
@@ -3,6 +3,15 @@ import "@fontsource/epilogue"; // Defaults to weight 400
 import "@fontsource/epilogue/400.css"; // Specify weight
 import "@fontsource/epilogue/400-italic.css";
 
+// LEXICAL STYLES 
+import "@/components/ui/lexicalEditor/ui/Button.css";
+import "@/components/ui/lexicalEditor/ui/ContentEditable.css";
+import "@/components/ui/lexicalEditor/ui/Dialog.css";
+import "@/components/ui/lexicalEditor/ui/Input.css";
+import "@/components/ui/lexicalEditor/ui/Modal.css";
+import "@/components/ui/lexicalEditor/nodes/ImageNode.css";
+import "@/components/ui/lexicalEditor/styles.css";
+
 import ModalManager from "@/components/ui/modals/ModalManager";
 import FooterNewsletter from "@/components/layout/Footer/FooterNewsletter";
 import { Providers } from "@/app/providers";
