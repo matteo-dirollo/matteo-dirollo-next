bba87583322930fa5acf9a5a0d8c98b62665d7bf
Matteo Di Rollo <matteo.dirollo@icloud.com>
2023-12-22 23:53:46 +0100

    Forcing colormode

--- src/app/(public)/layout.jsx
+++ src/app/(public)/layout.jsx
@@ -1,7 +1,7 @@
 // import "./globals.css";
-import '../../styles/fonts.css';
+import "../../styles/fonts.css";
 
-// LEXICAL STYLES 
+// LEXICAL STYLES
 import "@/components/ui/lexicalEditor/ui/Button.css";
 import "@/components/ui/lexicalEditor/ui/ContentEditable.css";
 import "@/components/ui/lexicalEditor/ui/Dialog.css";
@@ -16,14 +16,12 @@ import { Providers } from "@/app/providers";
 import PublicNav from "@/components/layout/Navbar/PublicNav";
 
 export default function RootLayout({ children }) {
-  
   return (
     <html lang="en">
       <body>
         <Providers>
           <PublicNav />
           <ModalManager />
-         
           {children}
           <FooterNewsletter />
         </Providers>
