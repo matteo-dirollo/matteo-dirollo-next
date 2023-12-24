d20b3f61aeec54cc500e5e32c02de4ef557db7bd
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2023-12-25 00:47:33 +0100

    Text tag for the spotify playlist

--- src/components/layout/Spotify/SpotifyPlaylist.jsx
+++ src/components/layout/Spotify/SpotifyPlaylist.jsx
@@ -11,7 +11,7 @@ const SpotifyPlaylist = () => {
         My spotify playlist
       </Heading>
       <Divider borderWidth="1.5px" borderColor="#000" mb="14px" />
-      <Text>Some tunes that helped me coding this application</Text>
+      <Text>Music I listened to while coding this application</Text>
       <Box width='100%' mt={10} mb={20}>
       <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/playlist/3rRMSaQGXezre7OUJnqrXy?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
       </Box>
