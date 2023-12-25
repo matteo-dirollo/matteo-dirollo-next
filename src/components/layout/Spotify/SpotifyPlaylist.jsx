fbba57794916a0631d4517795cabace5fd6d4537
Matteo Di Rollo <matteo.dirollo@icloud.com>
2023-12-25 21:26:58 +0100

    Homepage preview metadata modified

--- src/components/layout/Spotify/SpotifyPlaylist.jsx
+++ src/components/layout/Spotify/SpotifyPlaylist.jsx
@@ -13,16 +13,17 @@ const SpotifyPlaylist = () => {
 
   return (
     <Box width="100%">
-      <Heading mb={5} as="h2" size="xl" fontWeight="medium">
-        Music I listen to while coding
+      <Heading my={5} as="h2" size="xl" fontWeight="medium">
+        Music I listened to while coding this website
       </Heading>
       <Divider borderWidth="1.5px" borderColor="#000" mb="14px" />
       <Box width='100%' my={20}>
         <SpotifyPlayer
-          uri="spotify:playlist:3rRMSaQGXezre7OUJnqrXy"
+          uri="spotify:playlist:3rRMSaQGXezre7OUJnqrXy?utm_source=generator&theme=0"
           size={size}
           view={view}
           theme={theme}
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"
         />
       </Box>
     </Box>
@@ -30,3 +31,5 @@ const SpotifyPlaylist = () => {
 };
 
 export default SpotifyPlaylist;
+
+{/* <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3rRMSaQGXezre7OUJnqrXy?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> */}
