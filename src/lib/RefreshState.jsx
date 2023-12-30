cf0d87004e3e509cd28017dab15355076788154d
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2023-12-30 19:25:04 +0100

    Avoid multiple state copies

--- src/lib/RefreshState.jsx
+++ src/lib/RefreshState.jsx
@@ -4,12 +4,19 @@ import { useDispatch } from "react-redux";
 import { fetchUsers } from "@/api/auth/authSlice";
 
 export default function RefreshState({ children }) {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchPosts());
-        dispatch(fetchComments());
-        dispatch(fetchUsers())
-    }, [dispatch])
-    
+  const dispatch = useDispatch();
+  // const posts = useSelector((state) => state.posts); // Adjust this selector based on your actual state structure
+  // const comments = useSelector((state) => state.comments); // Adjust this selector based on your actual state structure
+  // const users = useSelector((state) => state.users); // Adjust this selector based on your actual state structure
+  const [dataLoaded, setDataLoaded] = useState(false);
+  useEffect(() => {
+    if (!dataLoaded) {
+      dispatch(fetchPosts());
+      dispatch(fetchComments());
+      dispatch(fetchUsers());
+      setDataLoaded(true);
+    }
+  }, [dispatch, dataLoaded]);
+
   return <div>{children}</div>;
 }
