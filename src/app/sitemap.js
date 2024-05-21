b29e8a376fd727dd2054515d696ca3b7e250a53b
Matteo Di Rollo <matteo.dirollo@icloud.com>
2024-05-21 11:32:19 +0200

    New sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,64 +1,90 @@
-import { fetchPosts } from "@/app/(public)/projects/postsSlice";
-import { store } from "@/lib/store";
 
-
-// Function to fetch project IDs using Redux
-async function fetchProjectIds() {
-  await store.dispatch(fetchPosts());
-  const articles = store.getState().posts.posts;
-  return articles.map(article => article.id);
-}
-
-// API route handler for GET requests
-export async function GET(req, res) {
-  const projectIds = await fetchProjectIds();
-  const projectUrls = projectIds.map(id => ({
-    url: `https://matteo-dirollo.com/projects/${id}`,
-    lastModified: new Date().toISOString(),
-    changeFrequency: 'monthly',
-    priority: 0.8,
-  }));
-
-  const urls = [
+export default function sitemap() {
+  return [
     {
       url: 'https://matteo-dirollo.com',
-      lastModified: new Date().toISOString(),
+      lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
       url: 'https://matteo-dirollo.com/projects',
-      lastModified: new Date().toISOString(),
+      lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: 'https://matteo-dirollo.com/contact',
-      lastModified: new Date().toISOString(),
+      lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
     },
-    ...projectUrls,
-  ];
-
-  const sitemapXml = `
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${urls
-        .map(url => `
-          <url>
-            <loc>${url.url}</loc>
-            <lastmod>${url.lastModified}</lastmod>
-            <changefreq>${url.changeFrequency}</changefreq>
-            <priority>${url.priority}</priority>
-          </url>
-        `)
-        .join('')}
-    </urlset>
-  `.trim();
-
-  return new Response(sitemapXml, {
-    headers: {
-      'Content-Type': 'application/xml',
-    },
-  });
+  ]
 }
+
+
+
+// import { fetchPosts } from "@/app/(public)/projects/postsSlice";
+// import { store } from "@/lib/store";
+
+
+// // Function to fetch project IDs using Redux
+// async function fetchProjectIds() {
+//   await store.dispatch(fetchPosts());
+//   const articles = store.getState().posts.posts;
+//   return articles.map(article => article.id);
+// }
+
+// // API route handler for GET requests
+// export async function GET(req, res) {
+//   const projectIds = await fetchProjectIds();
+//   const projectUrls = projectIds.map(id => ({
+//     url: `https://matteo-dirollo.com/projects/${id}`,
+//     lastModified: new Date().toISOString(),
+//     changeFrequency: 'monthly',
+//     priority: 0.8,
+//   }));
+
+//   const urls = [
+//     {
+//       url: 'https://matteo-dirollo.com',
+//       lastModified: new Date().toISOString(),
+//       changeFrequency: 'yearly',
+//       priority: 1,
+//     },
+//     {
+//       url: 'https://matteo-dirollo.com/projects',
+//       lastModified: new Date().toISOString(),
+//       changeFrequency: 'monthly',
+//       priority: 0.8,
+//     },
+//     {
+//       url: 'https://matteo-dirollo.com/contact',
+//       lastModified: new Date().toISOString(),
+//       changeFrequency: 'weekly',
+//       priority: 0.5,
+//     },
+//     ...projectUrls,
+//   ];
+
+//   const sitemapXml = `
+//     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+//       ${urls
+//         .map(url => `
+//           <url>
+//             <loc>${url.url}</loc>
+//             <lastmod>${url.lastModified}</lastmod>
+//             <changefreq>${url.changeFrequency}</changefreq>
+//             <priority>${url.priority}</priority>
+//           </url>
+//         `)
+//         .join('')}
+//     </urlset>
+//   `.trim();
+
+//   return new Response(sitemapXml, {
+//     headers: {
+//       'Content-Type': 'application/xml',
+//     },
+//   });
+// }
