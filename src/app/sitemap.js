ae1a91141fe33128fde5050717346064298d065c
Matteo Di Rollo <matteo.dirollo@icloud.com>
2024-05-21 11:57:55 +0200

    New sitemap v2

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,6 +1,20 @@
 
-export default function sitemap() {
-  return [
+import { fetchPosts } from "@/app/(public)/projects/postsSlice";
+import { store } from "@/lib/store";
+
+// Function to fetch project IDs using Redux
+async function fetchProjectIds() {
+  await store.dispatch(fetchPosts());
+  const articles = store.getState().posts.posts;
+  return articles.map(article => article.id);
+}
+
+export default async function sitemap() {
+  // Fetch dynamic project IDs
+  const projectIds = await fetchProjectIds();
+
+  // Static sitemap entries
+  const staticEntries = [
     {
       url: 'https://matteo-dirollo.com',
       lastModified: new Date(),
@@ -8,18 +22,29 @@ export default function sitemap() {
       priority: 1,
     },
     {
-      url: 'https://matteo-dirollo.com/projects',
+      url: `https://matteo-dirollo.com/projects`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://matteo-dirollo.com/contact',
+      url: `https://matteo-dirollo.com/contact`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
     },
-  ]
+  ];
+
+  // Dynamic project entries
+  const projectEntries = projectIds.map(id => ({
+    url: `https://matteo-dirollo.com/projects/${id}`,
+    lastModified: new Date(),  // You can customize this if you have individual lastModified dates for each project
+    changeFrequency: 'monthly', // Adjust as necessary
+    priority: 0.7,  // Adjust as necessary
+  }));
+
+  // Combine static and dynamic entries
+  return [...staticEntries, ...projectEntries];
 }
 
 
