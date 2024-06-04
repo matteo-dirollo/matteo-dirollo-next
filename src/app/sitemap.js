5e46e9ebc484f7b38c6e996910c1ca98f4438c4e
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2024-06-04 14:14:52 +0200

    temporary sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -62,3 +62,25 @@
 //     },
 //   });
 // }
+export default function sitemap() {
+  return [
+    {
+      url: 'https://matteo-dirollo.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://matteo-dirollo/projects',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://matteo-dirollo/contact',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}
