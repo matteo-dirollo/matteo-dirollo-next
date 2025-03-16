2c184bdf6b212a6a4ef684bcc0a62105ac5a3195
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2025-03-16 18:26:35 +0100

    New fix on sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -46,32 +46,40 @@ export default async function sitemap() {
       (item.date?.seconds || 0) * 1000 + (item.date?.nanoseconds || 0) / 1000000
     ).toISOString();
 
-    // Build the XML for a single post
-    return `
-      <url>
-        <loc>${getBaseUrl()}/projects/${item.id}</loc>
-        <lastmod>${lastModified}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>1</priority>
-        <news:news>
-          <news:publication>
-            <news:name>MDR</news:name>
-            <news:language>en</news:language>
-          </news:publication>
-          <news:publication_date>${lastModified}</news:publication_date>
-          <news:title>${item.title}</news:title>
-          <news:keywords>${item.tags?.join(", ") || ""}</news:keywords>
-        </news:news>
-        <image:image>
-          <image:loc>${item.imageUrl}</image:loc>
-          <image:caption>${item.title}</image:caption>
-        </image:image>
-        <description>${escapeXml(truncatedArticleDescription)}</description>
-      </url>
-    `;
+    return {
+      url: `${getBaseUrl()}/projects/${item.id}`,
+      lastModified: lastModified,
+      changeFrequency: "monthly",
+      priority: 1,
+      alternateRefs: [
+        {
+          rel: "news",
+          content: {
+            publication: {
+              name: "MDR",
+              language: "en",
+            },
+            publication_date: lastModified,
+            title: item.title,
+            keywords: item.tags?.join(", ") || "",
+          },
+        },
+        {
+          rel: "image",
+          content: {
+            loc: item.imageUrl,
+            caption: item.title,
+          },
+        },
+        {
+          rel: "description",
+          content: truncatedArticleDescription,
+        },
+      ],
+    };
   });
 
-  const postXml = await Promise.all(postPromises);
+  const post = await Promise.all(postPromises);
 
   const staticPages = [
     {
@@ -94,49 +102,5 @@ export default async function sitemap() {
     },
   ];
 
-  const staticXml = staticPages.map((page) => {
-    return `
-      <url>
-        <loc>${page.url}</loc>
-        <lastmod>${page.lastModified}</lastmod>
-        <changefreq>${page.changeFrequency}</changefreq>
-        <priority>${page.priority}</priority>
-      </url>
-    `;
-  });
-
-  // Build the complete XML string
-  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-            xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
-            xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-      ${staticXml.join("")}
-      ${postXml.join("")}
-    </urlset>
-  `;
-
-  return new Response(sitemapXml, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
-  });
-}
-
-// Helper function to escape XML characters
-function escapeXml(unsafe) {
-  return unsafe.replace(/[<>&'"]/g, function (c) {
-    switch (c) {
-      case "<":
-        return "&lt;";
-      case ">":
-        return "&gt;";
-      case "&":
-        return "&amp;";
-      case "'":
-        return "&apos;";
-      case '"':
-        return "&quot;";
-    }
-    return c;
-  });
+  return [...staticPages, ...post];
 }
