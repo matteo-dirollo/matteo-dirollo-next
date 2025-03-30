9c720ee2f73e66f4a70341e3aab9590cfe896dcb
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2025-03-30 21:09:23 +0200

    heroui development

--- src/app/providers.js
+++ src/app/providers.js
@@ -7,15 +7,18 @@ import ReduxProvider from "@/lib/ReduxProvider";
 import { ColorModeScript } from '@chakra-ui/react'
 import RefreshState from "../lib/RefreshState";
 import { HeroUIProvider } from '@heroui/system';
+import {useRouter} from "next/navigation";
+
 
 
 export function Providers({ children }) {
+  const router = useRouter();
   return (
     <ReduxProvider>
       <CacheProvider>
         <RefreshState>
         <ChakraProvider theme={theme}>
-        <HeroUIProvider>
+        <HeroUIProvider navigate={router.push}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           {children}
           </HeroUIProvider>
