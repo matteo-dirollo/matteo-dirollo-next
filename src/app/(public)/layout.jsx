9d3e375135171574a7686cfec8b9043875fcc9d7
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2025-03-30 19:06:18 +0200

    GA4 tag manager

--- src/app/(public)/layout.jsx
+++ src/app/(public)/layout.jsx
@@ -14,10 +14,12 @@ import ModalManager from "@/components/ui/modals/ModalManager";
 import FooterNewsletter from "@/components/layout/Footer/FooterNewsletter";
 import { Providers } from "@/app/providers";
 import PublicNav from "@/components/layout/Navbar/PublicNav";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID} />
       <body>
         <Providers>
           <PublicNav />
