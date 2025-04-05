5a7166bb9d18e6c5431028a29982f92f623a35ea
Matteo Di Rollo <matteo.dirollo@icloud.com>
2025-04-05 16:03:10 +0200

    formatting

--- src/components/ui/text/headings/ArticleHeading.jsx
+++ src/components/ui/text/headings/ArticleHeading.jsx
@@ -4,9 +4,8 @@ import React from "react";
 const ArticleHeading = ({ title }) => {
   return (
     <h1 className="my-4 text-4xl font-medium leading-tight text-gray-800 dark:text-gray-100">
-    {title}
-  </h1>
-
+      {title}
+    </h1>
   );
 };
 
