3a6c55d9f3b618c8356646cf828258a6e46f5106
Matteo Di Rollo <matteo.dirollo@icloud.com>
2025-04-09 00:33:02 +0200

    Tradedoubler tag

--- src/app/(public)/layout.jsx
+++ src/app/(public)/layout.jsx
@@ -1,5 +1,6 @@
 import "../globals.css";
 import "../../styles/fonts.css";
+import Head from "next/head";
 
 // LEXICAL STYLES
 import "@/components/ui/lexicalEditor/ui/Button.css";
@@ -14,16 +15,23 @@ import ModalManager from "@/components/ui/modals/ModalManager";
 import FooterNewsletter from "@/components/layout/Footer/FooterNewsletter";
 import { Providers } from "@/app/providers";
 import PublicNav from "@/components/layout/Navbar/PublicNav";
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-            <head>
-        {/* Tradedoubler site verification */}
-        <!--Tradedoubler site verification 3424982-->
-        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID} />
-      </head>
+      <Head>
+        {/* Injecting raw HTML comment */}
+        <meta charSet="UTF-8" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `<!--Tradedoubler site verification 3425221-->`,
+          }}
+        />
+        <GoogleTagManager
+          gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}
+        />
+      </Head>
       <body>
         <Providers>
           <PublicNav />
